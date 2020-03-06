6261219bf7890c1f043149af715645cf8cbce190
blanquez <ablanquezp@gmail.com>
2020-03-06 02:32:28 +0100

    primera implementacion de greedy, aun no funcional

--- clustering.py
+++ clustering.py
@@ -1,6 +1,6 @@
 import numpy as np
-import random
-from sklearn.utils import shuffle
+#from sklearn.cluster import KMeans
+import matplotlib.pyplot as plt
 
 #Use bucles(a mano) y csv, pandas es el mas sencillo y el mas elegante
 
@@ -21,39 +21,57 @@ cl_set = np.loadtxt("data/iris_set.dat", delimiter=',')
 #rand_set_const_20 = pd.read_csv("data/rand_set_const_20.const", delimiter=',', header=None)
 
 k=3
-dimension=4
+dimension=len(cl_set[1])
 seed=532020
-random.seed(seed)
+np.random.seed(seed)
 #cl_set=shuffle(cl_set, random_state=seed)  #Este es el unico shuffle que funciona en matrices
 #cl_set.reset_index(inplace=True, drop=True)  #Cuidado con los shuffles, que las restricciones tb
 
 #Minimos y maximos
 
-minimos = []
-maximos = []
+minimos = np.amin(cl_set, axis=0)
+maximos = np.amax(cl_set, axis=0)
+rango = np.subtract(maximos,minimos)
 
-for i in range(0,dimension):
-    minimos.append(int(min(cl_set[i])))
-    maximos.append(int(max(cl_set[i])))
+#Generar centroides y clusters
 
-#Generar centroides
+#c = []
+centroides = (rango*np.random.random((k,dimension)))+minimos
+print(centroides)
 
-centroides = []
-c = []
+    
+#for i in range(0,k):
+#    c.append([])
 
-for i in range(0,k):
-    aux = []
-    for j in range(0,dimension):
-        aux.append(random.randint(minimos[j], maximos[j]))
-    centroides.append(aux.copy())
-    c.append([])
+#Algoritmo k-medias
 
-#Funciones
+#kmeans = KMeans(n_clusters=k).fit(cl_set)
+#labels = kmeans.predict(cl_set)
+#centroid = kmeans.cluster_centers_
 
-def recalcular_centroides(c, cl_set):
-    centroides = []
-    for i in range(0,len(c)):
-        centroides[i] = c[i].mean()
+while True:
+    c = np.argmin(((cl_set[:, :, None] - centroides.T[None, :, :])**2).sum(axis=1), axis=1)
+    nuevos_centroides = np.array([cl_set[c == j, :].mean(axis=0) for j in range(k)])
+    if(nuevos_centroides == centroides).all:
+        break
+    else:
+        centroides = nuevos_centroides
 
-print(cl_set)
+colores=['red','green','blue']
+asignar=[]
 
+for row in c:
+        asignar.append(colores[row])
+print(centroides)
+
+plt.scatter(cl_set[:,2],cl_set[:,3],c=asignar)
+plt.scatter(centroides[:,2],centroides[:,3],marker='*', c=colores, s=1000)
+plt.show()
+#ax = Axes3D(fig)
+
+#def recalcular_centroides(c):
+#    for i in k:
+#        centroides[i] = np.linalg.norm(c[i])
+#    return centroides
+
+#def infeasibility
